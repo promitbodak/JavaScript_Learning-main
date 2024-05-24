6961684cbe2090daeedac7a87a81623dacd45f18
Promit <Promitbodak@gmail.com>
2024-05-24 21:56:07 +0530

    dataTypes commited

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all JS code as newer version
+
+let name = "promit";
+
+let age = 18;
+
+let isLoggedIn = false;
+
+// number => 2 to power 53
+// bigint
+// string => " "
+// boolean => true/false
+// null => standalone value (defines empty)
+// undefined =>  value not assigned yet
+// symbol => unique
+
+// object
+
+console.log(typeof "promit");
+console.log(typeof age);
+console.log(typeof isLoggedIn);
+console.log(typeof null); // object
+console.log(typeof undefined); // undifined
