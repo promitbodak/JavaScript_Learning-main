5b4b295070dec43c175daf0b540a5c1261df716c
Promit <Promitbodak@gmail.com>
2024-06-08 22:17:02 +0530

    function update

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -1,11 +1,10 @@
-
-function sayMyName(){
-    console.log("H");
-    console.log("I");
-    console.log("T");
-    console.log("E");
-    console.log("S");
-    console.log("H");
+function sayMyName() {
+  console.log("H");
+  console.log("I");
+  console.log("T");
+  console.log("E");
+  console.log("S");
+  console.log("H");
 }
 
 // sayMyName()
@@ -15,25 +14,60 @@ function sayMyName(){
 //     console.log(number1 + number2);
 // }
 
-function addTwoNumbers(number1, number2){
-
-    // let result = number1 + number2
-    // return result
-    return number1 + number2
+function addTwoNumbers(number1, number2) {
+  // let result = number1 + number2
+  // return result
+  return number1 + number2;
 }
 
-const result = addTwoNumbers(3, 5)
+const result = addTwoNumbers(3, 5);
 
 // console.log("Result: ", result);
 
-
-function loginUserMessage(username = "sam"){
-    if(!username){
-        console.log("PLease enter a username");
-        return
-    }
-    return `${username} just logged in`
+function loginUserMessage(username = "sam") {
+  if (!username) {
+    console.log("PLease enter a username");
+    return;
+  }
+  return `${username} just logged in`;
 }
 
 // console.log(loginUserMessage("hitesh"))
 // console.log(loginUserMessage("hitesh"))
+
+function calculateCartPrice(...num1) {
+  // ...num1 <-- REST OPERATOR
+  return num1;
+}
+console.log(calculateCartPrice(200, 400, 600));
+
+// CREATING OBJECT TO PASS INTO THE FUNCTION
+const user = {
+  username: "Promit",
+  price: 199,
+};
+
+function handleObject(anyObject) {
+  console.log(
+    `Username is ${anyObject.username} and price is ${anyObject.price}`
+  );
+}
+
+handleObject(user); // Jaroori nahi ki object pahle se banake hi pass karna padega
+
+handleObject({
+  // you can pass it on the go also
+  username: "Ban2Kolkata",
+  price: 6969,
+});
+
+// PASSING ARRAYS
+
+const myNewArray = [200, 400, 100, 600];
+
+function returnSecondValue(getArray) {
+  return getArray[1];
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 500, 1000]));
